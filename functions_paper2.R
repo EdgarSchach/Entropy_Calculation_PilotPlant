3399ccd88e63587de879a3746d46f10ae7b98796
Edgar Schach <edgar.schach89@gmail.com>
2024-06-02 21:54:27 +0200

    final calculations part 2

--- functions_paper2.R
+++ functions_paper2.R
@@ -51,124 +51,96 @@ entropyforboots.corrected <- function(feed,products,weights,recal = T, con.mass.
   #CALCULATIONS FOR FEEDSTAGE
   
   # extensive properties for feed stage:
-  
+
   Fi00 <- lapply(feed,sum)
   Fi0k <- lapply(feed,rowSums)
   Fij0 <- lapply(feed,colSums) # this might be corrected later on
   Fijk <- feed
-  
   # calculation of intensive properties:
-  
-  
-  
   if (con.mass.balance == F){
-    cij0 <- map(Fij0,~.x/sum(.x)) 
+    cij0 <- map(Fij0,~.x/sum(.x))
   }else{
     if(recal == T){
       cij0 <- t(grouped_concentrations[names(products),])
       cij0 <- cij0 %*% weights[names(products)]/100
-      cij0 <- list(recal_feed = as.data.frame(t(cij0))) 
+      cij0 <- list(recal_feed = as.data.frame(t(cij0)))
     }else{
       cij0 <- map(names(feed),~grouped_concentrations[.x,]/100)
     }
-    # con_weighting <- map(cij0,~1/(1-.x$Rest)) 
+    # con_weighting <- map(cij0,~1/(1-.x$Rest))
     # cij0 <- map(cij0,~select(.x,-Rest))
-    cij0 <- map(cij0,unlist) 
-    
+    cij0 <- map(cij0,unlist)
   }
-  
   cijk <- map2(Fijk,Fi0k,~sweep(.x,1,.y,"/")) #integrowth of product stage
-  
   if (recal == T){
     ti00 <-  1
   }else{
     vm <- vol_masses[names(feed)]
     vm <- map2(vm,idx[names(feed)],~.x[.y,])
     vm <- map(vm,~sum(.x$mass_vol))
-    ti00 <- clo(unlist(vm)) 
+    ti00 <- clo(unlist(vm))
   }
-  
   ti001 <- weights[names(feed)]
-  
   pi0k <- map(Fi0k,~.x/sum(.x))  # mass distribution in product stage
   pijk <- map2(Fijk, Fij0, ~ sweep(.x,2,.y,"/")) #component distribution in product stage
   pijk <- map(pijk,~mutate_all(.x,~replace(., is.na(.), 0)))
   # pijk <- map(pijk,~select(.x,-Rest))
-  
   ent_ti00 <- .entropy(ti00)
-  if(con.corr == T){
-    ent_cij0 <- sapply(cij0,.entropy) *  unlist(con_weighting)
+  if(con.mass.balance == T){
+    ent_cij0 <- sapply(cij0,.entropy)
     ent_cij0 <- ti00 %*% ent_cij0
-    pijk <- map(pijk,~select(.x,-Rest))
   }else{
-    ent_cij0 <- ti00 %*% sapply(cij0,.entropy) 
+    ent_cij0 <- ti00 %*% sapply(cij0,.entropy)
   }
-  
   ent_pijk <- lapply(pijk,function(x)apply(x,2,.entropy))
   ent_pijk <- map2(cij0,ent_pijk,~.x%*%.y,)
   ent_pijk <- ti00 %*% unlist(ent_pijk)
-  
   ti00.cij0.pijk <- c(ti00 = ent_ti00, cij0 = ent_cij0, pijk = ent_pijk)
-  
   #ti00.pi0k.cijk
-  
   ent_ti00 <- .entropy(ti00)
   ent_pi0k <- sapply(pi0k,.entropy)
   ent_pi0k <- ti00 %*% ent_pi0k
   ent_cijk <- lapply(cijk,function(x)apply(x,1,.entropy))
   ent_cijk <- map2(pi0k,ent_cijk,~.x%*%.y)
   ent_cijk <- ti00 %*% unlist(ent_cijk)
-  
   ti00.pi0k.cijk <- c(ti00 = ent_ti00,pi0k = ent_pi0k,cijk = ent_cijk)
-  
   res_feedstage <- list(ti00.cij0.pijk = ti00.cij0.pijk,
                         ti00.pi0k.cijk = ti00.pi0k.cijk)
-  res_feedstage <- bind_rows(res_feedstage,.id = "aggregation") %>% 
+  res_feedstage <- bind_rows(res_feedstage,.id = "aggregation") %>%
     gather(key = "entropy_type",value = "entropy",-aggregation) %>% na.omit()
   
   # CALCULATION FOR PRODUCT STAGE####
   
   Pi00 <- lapply(products,sum)
   Pi0k <- lapply(products,rowSums)
-  Pij0 <- lapply(products,colSums) 
+  Pij0 <- lapply(products,colSums)
   Pijk <- products
-  
-  if (con.corr == F){
-    cij0 <- map(Pij0,~.x/sum(.x)) 
+  if (con.mass.balance == F){
+    cij0 <- map(Pij0,~.x/sum(.x))
   }else{
     cij0 <- map(names(products),~grouped_concentrations[.x,]/100)
-    cij0 <- map(cij0,unlist) 
+    cij0 <- map(cij0,unlist)
   }
-  
   cijk <- map2(Pijk,Pi0k,~sweep(.x,1,.y,"/")) #integrowth of product stage
-  
   vm <- vol_masses[names(products)]
   vm <- map2(vm,idx[names(products)],~.x[.y,])
   vm <- map(vm,~sum(.x$mass_vol))
-  
   ti00 <- clo(unlist(vm))
   ti001 <- weights[names(products)]
-  
   pi0k <- map(Pi0k,~.x/sum(.x))  # mass distribution in product stage
   pijk <- map2(Pijk, Pij0, ~ sweep(.x,2,.y,"/")) #component distribution in product stage
   pijk <- map(pijk,~mutate_all(.x,~replace(., is.na(.), 0)))
-  
-  
   #ti00.cij0.pijk
-  
   ent_ti00 <- .entropy(ti00)
-  if(con.corr == T){
-    ent_cij0 <- sapply(cij0,.entropy) *  unlist(con_weighting)
+  if(con.mass.balance == T){
+    ent_cij0 <- sapply(cij0,.entropy)
     ent_cij0 <- ti00 %*% ent_cij0
-    pijk <- map(pijk,~select(.x,-Rest))
   }else{
     ent_cij0 <- ti00 %*% sapply(cij0,.entropy)
   }
-  
   ent_pijk <- lapply(pijk,function(x)apply(x,2,.entropy))
   ent_pijk <- map2(cij0,ent_pijk,~.x%*%.y,)
   ent_pijk <- ti00 %*% unlist(ent_pijk)
-  
   ti00.cij0.pijk <- c(ti00 = ent_ti00, cij0 = ent_cij0, pijk = ent_pijk)
   
   #ti00.pi0k.cijk
@@ -180,16 +152,11 @@ entropyforboots.corrected <- function(feed,products,weights,recal = T, con.mass.
   ent_cijk <- map2(pi0k,ent_cijk,~.x%*%.y)
   ent_cijk <- ti00 %*% unlist(ent_cijk)
   ti00.pi0k.cijk <- c(ti00 = ent_ti00,pi0k = ent_pi0k,cijk = ent_cijk)
-  
   res_productstage <- list(ti00.cij0.pijk = ti00.cij0.pijk,
                            ti00.pi0k.cijk = ti00.pi0k.cijk)
-  
-  res_productstage <- bind_rows(res_productstage,.id = "aggregation") %>% 
+  res_productstage <- bind_rows(res_productstage,.id = "aggregation") %>%
     gather(key = "entropy_type",value = "entropy",-aggregation) %>% na.omit()
-  
   res <-  bind_rows(feed= res_feedstage,product = res_productstage,.id = "stage")
-  
-  
   res <- list(entropy = res,
               t_val = ti00)
   return(res)
